3ebb6408549874c0b404da2351a4ae8327dcfc30
LucasTrinchieri <lucastrinchieri123@gmail.com>
2024-03-06 22:56:03 -0300

    build.sh script improved

--- build.sh
+++ build.sh
@@ -1,10 +1,33 @@
-# exit on error
-set -o errexit
+#!/bin/bash
 
-# dependencies
+# Función para manejar errores
+handle_error() {
+    local lineno="$1"
+    local message="$2"
+    echo "Error en la línea $lineno: $message"
+    # Salir con un código de error
+    exit 1
+}
+
+# Configurar manejo de errores
+trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
+
+# Instalar dependencias
 npm install
 
-# primsa ORM set up
-npx prisma generate
-npx prisma migrate deploy
+# Generar archivos de Prisma
+echo "Generando archivos de Prisma..."
+npx prisma generate || {
+    echo "Error al generar archivos de Prisma."
+    exit 1
+}
+
+# Aplicar migraciones de Prisma
+echo "Aplicando migraciones de Prisma..."
+npx prisma migrate deploy || {
+    echo "Error al aplicar migraciones de Prisma."
+    exit 1
+}
+
+echo "Despliegue completado con éxito."
 
